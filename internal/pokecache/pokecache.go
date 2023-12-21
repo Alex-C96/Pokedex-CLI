18b23567638bd25a260ec5f9f1abcc2781e2751e
Alex Clarke <alexander15clarke@gmail.com>
2023-12-20 22:42:02 -0600

    add caching, still need to integrate

---
+++ internal/pokecache/pokecache.go
@@ -0,0 +1,59 @@
+package pokecache
+
+import (
+	"sync"
+	"time"
+)
+
+type cacheEntry struct {
+	createdAt time.Time
+	val       []byte
+}
+
+type Cache struct {
+	entries  map[string]cacheEntry
+	mu       sync.Mutex
+	interval time.Duration
+}
+
+func NewCache(interval time.Duration) *Cache {
+	cache := &Cache{
+		entries:  make(map[string]cacheEntry),
+		mu:       sync.Mutex{},
+		interval: interval,
+	}
+	cache.reapLoop(interval)
+	return cache
+}
+
+func (c *Cache) Add(key string, val []byte) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.entries[key] = cacheEntry{
+		createdAt: time.Now().UTC(),
+		val:       val,
+	}
+}
+
+func (c *Cache) Get(key string) ([]byte, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	cacheE, ok := c.entries[key]
+	return cacheE.val, ok
+}
+
+func (c *Cache) reapLoop(interval time.Duration) {
+	ticker := time.NewTicker(interval)
+	for range ticker.C {
+		c.reap(interval)
+	}
+}
+
+func (c *Cache) reap(interval time.Duration) {
+	timeAgo := time.Now().UTC().Add(-interval)
+	for k, v := range c.entries {
+		if v.createdAt.Before(timeAgo) {
+			delete(c.entries, k)
+		}
+	}
+}
